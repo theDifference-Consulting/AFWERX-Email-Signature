bd855e188a9315c1f4119c6e768e1a6075488696
Kyle Schoenberger <kyleschoen@gmail.com>
2019-10-18 11:43:10 -0700

    initial upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const populate = function(inputElement, targetElement) {
+	//get text from current field
+	let input = document.getElementById(inputElement).value;
+
+	if (/^[A-z|À-Ö|Ø-ö|ø-ÿ|¨|\´|0-9|.|@|,|(|)|\-|\—|\—|\ |À-ú]*$/.test(input)) { //character whitelist
+   		document.getElementById(targetElement).innerHTML = input;
+		//change html in the sig box
+		if (targetElement === 'email') {	
+			document.getElementById(targetElement).href = 'mailto:' + input;
+		} else if (targetElement === 'phone') {
+			document.getElementById(targetElement).href = 'tel:' + input;
+		};
+	} else {
+		document.getElementById(inputElement).value = input.substring(0, input.length - 1);
+		alert('error: invalid character');
+	};
+
+	let tableWidth = document.getElementById("signature").clientWidth;
+	document.getElementById("signature").setAttribute('style', 'font-family:Roboto, Arial, san-serif; width:'+ tableWidth + ';');
+	console.log(sigWidth);
+}
+
+const copy = function() {
+	var copyText = document.querySelector("#signature");
+	window.getSelection().selectAllChildren(copyText);
+	document.execCommand("copy");
+	window.getSelection().removeAllRanges();
+	alert("Your signature has been copied to clipboard!");
+}
